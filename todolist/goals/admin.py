4dcc3b99c29058aa75770bc640e5b9c3a998eede
Vladislav <xxx.swat.ru@gmail.com>
2023-03-29 08:13:14 +0300

    lesson 37 fix admin.py

--- todolist/goals/admin.py
+++ todolist/goals/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from todolist.goals.models import GoalCategory, Goal, GoalComment
+from todolist.goals.models import GoalCategory, Goal, GoalComment, Board, BoardParticipant
 
 
 @admin.register(GoalCategory)
@@ -20,3 +20,14 @@ class GoalCommentsAdmin(admin.ModelAdmin):
     list_display = ("text", "created", "updated")
     search_fields = ("text",)
 
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ("title", "created", "updated")
+    search_fields = ("title",)
+
+
+@admin.register(BoardParticipant)
+class BoardParticipantAdmin(admin.ModelAdmin):
+    list_display = ("user", "created", "updated")
+    search_fields = ("user",)
